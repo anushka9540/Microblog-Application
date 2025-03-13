f8849802845354b03029e82ff390b9977c6d233a
Anushka Prajapati <anushkaprajapati827@gmail.com>
2025-03-13 15:47:52 +0530

    removed computed used post.length

--- src/composables/use-microblog.js
+++ src/composables/use-microblog.js
@@ -2,23 +2,12 @@ import { ref, computed } from 'vue';
 import postsData from '../data/posts.json';
 
 const posts = ref(postsData);
-
 const searchTerm = ref('');
 const filterTag = ref('');
 
 const setFilter = (tag) => {
   searchTerm.value = tag.toLowerCase();
 };
-
-const hashtagExists = computed(() => {
-  if (!searchTerm.value) return true;
-
-  const term = searchTerm.value.toLowerCase();
-
-  return posts.value.some(post =>
-    post.hashtags.some(tag => tag.toLowerCase().startsWith(term))
-  );
-});
 
 const filteredPosts = computed(() => {
   if (!searchTerm.value) return posts.value;
@@ -29,6 +18,8 @@ const filteredPosts = computed(() => {
     post.hashtags.some(tag => tag.toLowerCase().startsWith(term))
   );
 });
+
+const hashtagExists = computed(() => filteredPosts.value.length > 0);
 
 const incrementLike = (id) => {
   const post = posts.value.find(post => post.id === id);
